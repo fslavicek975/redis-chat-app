92f5a7d4f570599d6db3fb9120be63dd84a71872
fslavicek975 <fslavicek975@gmail.com>
2025-06-12 11:24:45 +0200

    add chat layout

---
+++ src/app/chat/ChatLayout.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { cn } from "@/lib/utils";
+import React, { useEffect, useState } from "react";
+
+interface ChatLayoutProps {
+  defaultLayout: number[] | undefined;
+}
+
+const ChatLayout = ({ defaultLayout = [320, 480] }: ChatLayoutProps) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenWidth = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // Set initial state
+    checkScreenWidth();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", checkScreenWidth);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", checkScreenWidth);
+    };
+  }, []);
+  return (
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="h-full items-stretch bg-background
+      rounded-lg"
+      onLayout={(sizes: number[]) => {
+        document.cookie = `react-resizable-panels:layout=${JSON.stringify(
+          sizes
+        )}; `;
+      }}
+    >
+      <ResizablePanel
+        defaultSize={defaultLayout[0]}
+        collapsedSize={8}
+        collapsible={true}
+        minSize={isMobile ? 0 : 24}
+        maxSize={isMobile ? 0 : 30}
+        onCollapse={() => {
+          setIsCollapsed(true);
+          document.cookie = `react-resizable-panels:collapsed=true; `;
+        }}
+        onExpand={() => {
+          setIsCollapsed(false);
+          document.cookie = `react-resizable-panels:collapsed=fakse; `;
+        }}
+        className={cn(
+          isCollapsed && "min-w-[80px] transition-all duration-300 ease-in-out"
+        )}
+      >
+        Sidebar
+      </ResizablePanel>
+      <ResizableHandle withHandle></ResizableHandle>
+      <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
+        <div className="flex justify-center items-center h-full w-full px-19">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <img
+              src="/logo.png"
+              alt="Logo"
+              className="w-full md:w-2/3 lg:w-1/2"
+            />
+            <p className="text-muted-foreground text-center">
+              Click on a chat to view the messages
+            </p>
+          </div>
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+};
+
+export default ChatLayout;
